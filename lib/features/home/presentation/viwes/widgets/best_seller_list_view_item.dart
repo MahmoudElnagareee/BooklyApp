39501ceaee5fffd532aac9eac3bdc8595e9b9722
Mahmoud Elnagar <your-email@example.com>
2025-09-20 20:09:37 +0300

    best seller item done

--- lib/features/home/presentation/viwes/widgets/best_seller_list_view_item.dart
+++ lib/features/home/presentation/viwes/widgets/best_seller_list_view_item.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app/constant.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/presentation/viwes/widgets/book_rating.dart';
 import 'package:flutter/material.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
@@ -25,34 +26,39 @@ class BestSellerListViewItem extends StatelessWidget {
             ),
           ),
           const SizedBox(width: 30,),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.5,
-                child:  Text(
-                  'Harry Potter and the Global of Fire',
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: Styles.textStyle20.copyWith(fontFamily: KGtSectraFine),
-                ),
-              ),
-              const SizedBox(height: 3,),
-              Text(
-                'J.K. Rowling',
-                style: Styles.textStyle14.copyWith(color: Colors.white54),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  child:  Text(
+                    'Harry Potter and the Global of Fire',
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: Styles.textStyle20.copyWith(fontFamily: KGtSectraFine),
+                  ),
                 ),
                 const SizedBox(height: 3,),
-                Row(
-                  children: [
-                    Text(r'19.99$',style: Styles.textStyle20.copyWith(fontWeight: FontWeight.bold),),
-                    
-                  ],
-                ),
-            ],
+                Text(
+                  'J.K. Rowling',
+                  style: Styles.textStyle14.copyWith(color: Colors.white54),
+                  ),
+                  const SizedBox(height: 3,),
+                  Row(
+                    children: [
+                      Text(r'19.99$',style: Styles.textStyle20.copyWith(fontWeight: FontWeight.bold),),
+                      Spacer(),
+                      BookRating(),
+                    ],
+                  ),
+              ],
+            ),
           ),
         ],
       ),
     );
   }
 }
+
+
--- lib/features/home/presentation/viwes/widgets/home_view_body.dart
+++ lib/features/home/presentation/viwes/widgets/home_view_body.dart
@@ -26,3 +26,5 @@ class HomeViewBody extends StatelessWidget {
   }
 }
 
+
+
