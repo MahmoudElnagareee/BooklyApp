717185cd4bb96b72ed1269204615d822f6ec82b7
Mahmoud Elnagar <your-email@example.com>
2025-09-20 19:52:34 +0300

    best seller item part 3

---
+++ lib/features/home/presentation/viwes/widgets/best_seller_list_view_item.dart
@@ -0,0 +1,58 @@
+import 'package:bookly_app/constant.dart';
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 130,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                image: const DecorationImage(
+                  image: AssetImage(AssetsData.testImage),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(width: 30,),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.5,
+                child:  Text(
+                  'Harry Potter and the Global of Fire',
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: Styles.textStyle20.copyWith(fontFamily: KGtSectraFine),
+                ),
+              ),
+              const SizedBox(height: 3,),
+              Text(
+                'J.K. Rowling',
+                style: Styles.textStyle14.copyWith(color: Colors.white54),
+                ),
+                const SizedBox(height: 3,),
+                Row(
+                  children: [
+                    Text(r'19.99$',style: Styles.textStyle20.copyWith(fontWeight: FontWeight.bold),),
+                    
+                  ],
+                ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
