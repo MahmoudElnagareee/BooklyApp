b2db6bc544b9b706034407f70f8a9115218e38e6
Mahmoud Elnagar <your-email@example.com>
2025-09-23 16:16:55 +0300

    rfactor books details view

--- lib/features/home/presentation/viwes/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/viwes/widgets/book_details_view_body.dart
@@ -1,9 +1,6 @@
-import 'package:bookly_app/core/utils/styles.dart';
-import 'package:bookly_app/features/home/presentation/viwes/widgets/book_rating.dart';
-import 'package:bookly_app/features/home/presentation/viwes/widgets/books_action.dart';
+import 'package:bookly_app/features/home/presentation/viwes/widgets/books_details_section.dart';
 import 'package:bookly_app/features/home/presentation/viwes/widgets/custom_book_deatils_app_bar.dart';
-import 'package:bookly_app/features/home/presentation/viwes/widgets/custom_book_image.dart';
-import 'package:bookly_app/features/home/presentation/viwes/widgets/similar_books_list_view.dart';
+import 'package:bookly_app/features/home/presentation/viwes/widgets/similar_books_section.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -11,53 +8,26 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
-
     return CustomScrollView(
       slivers: [
         SliverFillRemaining(
           hasScrollBody: false,
           child: Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        children: [
-          CustomBokkDeatilsAppBar(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * 0.2),
-            child: CustomBookImage(),
-          ),
-          const SizedBox(height: 43),
-          Text('The Jungle Book', style: Styles.textStyle30),
-          const SizedBox(height: 6),
-          Opacity(
-            opacity: 0.7,
-            child: Text(
-              'Rudyard Kipling',
-              style: Styles.textStyle18.copyWith(
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.w500,
-              ),
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              children: const [
+                CustomBokkDeatilsAppBar(),
+                BookDetailsSection(),
+                 Expanded(child: SizedBox(height: 50)),
+                SimilarBooksSection(),
+              ],
             ),
           ),
-          const SizedBox(height: 18),
-          BookRating(mainAxisAlignment: MainAxisAlignment.center),
-          const SizedBox(height: 37),
-          const BooksAction(),
-          const Expanded(child: SizedBox(height: 50)),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'You can also like',
-              style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),
-            ),
-          ),
-          const SizedBox(height: 16),
-          SimilarBooksListView(),
-        ],
-      ),
-    ),
         ),
       ],
     );
   }
 }
+
+
+
